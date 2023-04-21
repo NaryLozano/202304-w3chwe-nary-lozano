04b97cb851a7a8f0c7b763f8daa626d310d981bf
Nary Lozano <narylozano@gmail.com>
2023-04-21 17:55:17 +0200

    Initial Commit

---
+++ src/ts/components/Components.ts
@@ -0,0 +1,20 @@
+import ComponentStructure from "../types";
+
+abstract class Component implements ComponentStructure {
+  domElement: HTMLElement;
+
+  constructor(
+    private readonly parentElement: HTMLElement,
+    className: string,
+    tag = "div"
+  ) {
+    this.domElement = document.createElement(tag);
+    this.domElement.className = className;
+
+    this.parentElement.appendChild(this.domElement);
+  }
+
+  abstract renderHtml(): void;
+}
+
+export default Component;
